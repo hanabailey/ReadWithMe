5df808395108cba0c62fd052969081ba253bc1a5
HANA CHOI <hana40820@gmail.com>
2023-07-03 11:03:59 +0900

    Changing Home Header style

--- src/app/components/HomeHeader.tsx
+++ src/app/components/HomeHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./HomeHeader.css";
+import styles from "./HomeHeader.module.scss";
 import Link from "next/link";
 import Image from 'next/image';
 import bookIcon from '../../../public/img/Vector.png';
@@ -8,17 +8,18 @@ import userIcon from '../../../public/img/userIcon.png'
 function HomeHeader() {
   return (
     <>
-      <header className="page-header">
-        <div className="page-header-item"><Image className="bookIcon" src={bookIcon} alt="" width={25} height={25}/></div>
-        <div className="page-header-item"><Link className="mainbtn" href="/home">Read with ME</Link></div>
-        <div className="page-header-item"><Image className='userIcon' src={userIcon} alt="" width={30} height={30}></Image></div>
-      </header>
-      <nav className="nav">
-        <div className="nav-item">Home</div>
-        <div className="nav-item">My Library</div>
-        <div className="nav-item">Calender</div>
-        <div className="nav-item">Search</div>
-        <div className="nav-item">Book club</div>
+    <header className={styles.pageHeader}>
+        <div><Image className="bookIcon" src={bookIcon} alt="" width={25} height={25}/></div>
+        <div><Link href="/home">Read with ME</Link></div>
+        <div><Image className='userIcon' src={userIcon} alt="" width={30} height={30}></Image></div>
+    </header>
+      
+      <nav className={styles.nav}>
+        <div><Link href='/home'>Home</Link></div>
+        <div><Link href='/myLibrary'>My Library</Link></div>
+        <div><Link href='/calander'>Calender</Link></div>
+        <div><Link href='/search'>Search</Link></div>
+        <div><Link href='/bookClub'>Book club</Link></div>
       </nav>
    
     </>

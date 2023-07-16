6db58fd97065c91183b82700681b210ca383db69
HANA CHOI <hana40820@gmail.com>
2023-07-16 14:36:24 +0900

    Tweaking config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,45 @@ const nextConfig = {
         hostname: "images.unsplash.com"
       }
     ]
-  }
+  },
+
+  // reactStrictMode: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: `https://openapi.naver.com/v1/search/book.json/:path*`,
+  //     },
+  //   ];
+  // },
+
+  
 }
+// module.exports = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'https://openapi.naver.com/v1/search/book.json/:path*', // 프록시할 API 도메인과 경로
+//       },
+//     ];
+//   },
+// };
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*', // API 경로에 따라 변경
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // 허용할 도메인을 지정 (모든 도메인 허용 시 '*')
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig

2ae848f14edca5a8e7e541e5d8f1355e0192afc5
HANA CHOI <hana40820@gmail.com>
2023-08-11 12:13:16 +0900

    Making division line component

---
+++ src/app/components/UI/DivisionLine.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '@/app/components/UI/DivisonLine.module.scss'
+
+function DivisionLine() {
+  return (
+    <div className={styles.jbDivisionLine}></div>
+  )
+}
+
+export default DivisionLine

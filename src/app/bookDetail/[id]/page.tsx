22863a49d10f9ebc9b5f86d9cf8bdc4fbbf84239
hanaBailey <hana40820@gmail.com>
2023-09-19 20:42:19 -0700

    Adding more details on book details page

--- src/app/bookDetail/[id]/page.tsx
+++ src/app/bookDetail/[id]/page.tsx
@@ -8,6 +8,8 @@ import DivisionLine from "@/app/components/UI/DivisionLine";
 //mentine
 import { Rating } from "@mantine/core";
 import { Select } from "@mantine/core";
+import { Textarea } from "@mantine/core";
+import { Switch } from "@mantine/core";
 
 function BookDetail(props) {
   const [fetchError, setFetchError] = useState(null);
@@ -84,35 +86,31 @@ function BookDetail(props) {
             >
               My
             </button>
-            {/* 현재 상태 보고 변경가능 버튼 */}
             <button
               className={activeTab === 1 ? styles.active : ""}
               onClick={() => handleTabClick(1)}
             >
               Rate
             </button>
-            {/* 별점, 추천여부 */}
             <button
               className={activeTab === 2 ? styles.active : ""}
               onClick={() => handleTabClick(2)}
             >
               Review
             </button>
-            {/* 한줄리뷰, 장문의 독후감 포함 */}
             <button
               className={activeTab === 3 ? styles.active : ""}
               onClick={() => handleTabClick(3)}
             >
               Timeline
             </button>
-            {/* 언제 서재에 넣었고, 다읽은 날, 독서상태 변경되면 타임라인에 반영 */}
             <button
               className={activeTab === 4 ? styles.active : ""}
               onClick={() => handleTabClick(4)}
             >
               Details
             </button>{" "}
-            {/* 페이지, 출판사,ISBN, 정가, 관련링크 */}
+          
             <button
               className={activeTab === 5 ? styles.active : ""}
               onClick={() => handleTabClick(5)}
@@ -122,8 +120,7 @@ function BookDetail(props) {
           </nav>
 
           <div className={styles.infoSection}>
-            <h3>MY</h3>
-
+            <h2 className={styles.infoHeader}>MY</h2>
             <Select
               label="Reading status"
               placeholder="Pick one"
@@ -136,12 +133,62 @@ function BookDetail(props) {
               ]}
             />
 
-            <Rating value={value} onChange={setValue} />
-            {/* TODO: 별점 바꾸면 디비에도 반영되게 해라 */}
-            <button onClick={() => setValue(0)}>reset</button>
-            <p>{books.user_books[0].reading_status}</p>
-            <p>{books.user_books[0].is_recommend}</p>
+            {/* 별점 */}
+            <h3 className={styles.infoSubHeader}>Rating</h3>
+            <div className={styles.ratingContainer}>
+              <Rating value={value} onChange={setValue} />
+              {/* TODO: 별점 바꾸면 디비에도 반영되게 해라 */}
+              <button
+                onClick={() => setValue(0)}
+                className={styles.enterButton}
+              >
+                reset
+              </button>
+            </div>
+
+            {/* 짧은리뷰 */}
+            <h3 className={styles.infoSubHeader}>Short Review</h3>
+            <p>
+              What do you think about this book? Write your short review down.
+            </p>
+            <Textarea
+              placeholder="Your short review"
+              label="Short review"
+              radius="md"
+              size="md"
+            />
+            <button className={styles.enterButton}>Enter</button>
+
+            {/* 마이픽 */}
+            <h3 className={styles.infoSubHeader}>My pick</h3>
+            <div className={styles.recommendationContainer}>
+              <p className={styles.recoMention}>Do you recommend this book?</p>
+              <Switch size="lg" />
+            </div>
+
+          <DivisionLine/>
+
+          <h2>Rate</h2>
+
+          <DivisionLine/>
+
+          <h2>Review</h2>
+          <DivisionLine/>
+
+{/* 언제 서재에 넣었고, 다읽은 날, 독서상태 변경되면 타임라인에 반영 */}
+          <h2>Timeline</h2>
+          <DivisionLine/>
+          
+  {/* 페이지, 출판사,ISBN, 정가, 관련링크 */}
+          <h2>Book Detail</h2>
+          <DivisionLine/>
+
+          <h2>Book description</h2>
+
+          <DivisionLine/>
+
           </div>
+
         </>
       )}
     </>
